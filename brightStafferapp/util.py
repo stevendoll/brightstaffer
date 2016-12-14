54f8fb51938abce07ba7242b47feaef5e12f67db
chandankiwitech <chandanvarma2@gmail.com>
2016-12-14 20:12:07 +0530

    changes in error code and messages

--- brightStafferapp/util.py
+++ brightStafferapp/util.py
@@ -4,64 +4,21 @@ import simplejson
 import datetime
 
 
-def returnIntegrityMessage(param_dict={}):
-    param_dict['message'] = 'Email id is already exist'
-    return returnresponsejson(param_dict)
+
+
+
+def returnErrorShorcut(error_code, error_Status, httpstatus=200):
+    output = {}
+    output['success'] = False
+    output['errorcode'] = error_code
+    output['errorstring'] = error_Status
+    return returnresponsejson(output, httpstatus)
+
 
 def returnSuccessShorcut(param_dict={}, httpstatus=200):
+    param_dict['message'] = 'success'
     return returnresponsejson(param_dict, httpstatus)
 
 def returnresponsejson(pass_dict, httpstatus=200):
     json_out = simplejson.dumps(pass_dict)
     return HttpResponse(json_out, status=httpstatus, content_type="application/json")
-
-def returnUnAuthorized(param_dict={}):
-    param_dict['message'] = 'UnAuthorized User'
-    return returnresponsejson(param_dict)
-
-def returnTokenTimeout(param_dict={}):
-    param_dict['message'] = 'Email link is no longer valid'
-    return returnresponsejson(param_dict)
-
-#
-# def retrunEmailError(param_dict={}):
-#     param_dict['message'] = 'Email id is not Registered'
-#     return returnresponsejson(param_dict)
-
-ERRORPAGE_MAPPING = {
-    400:{
-        'result':'',
-        'message':''
-    },
-    401:{
-        'result':'',
-        'message':''
-    },
-    403:{
-        'result':'',
-        'message':''
-    },
-    404:{
-        'result':'UnknownEntity',
-        'message':'UnknownEntity, Please try again.'
-    },
-    500:{
-        'result':'ServerError',
-        'message':'ServerError, Please try again.'
-    },
-    501:{
-        'result':'',
-        'message':''
-    },
-}
-
-
-def ReturnErrorResponse(status=404, **kwargs):
-    msg = kwargs.get('messgage')
-    data = {
-        'serverStatusCodes': status,
-        'currentDate': str(datetime.datetime.now()),
-        'result':ERRORPAGE_MAPPING[status]['result'] ,
-        'message':ERRORPAGE_MAPPING[status]['message']
-    }
-    return returnSuccessShorcut(data, httpstatus=status)
