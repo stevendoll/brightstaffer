ccf1f2d94ae1b3aa93f058c117ff073d36df9448
chandankiwitech <chandanvarma2@gmail.com>
2017-02-24 18:42:18 +0530

    Create Models and admin interface

--- brightStafferapp/models.py
+++ brightStafferapp/models.py
@@ -56,11 +56,31 @@ class Concept(models.Model):
         return str(self.concept)
 
 
+def get_upload_file_name(instance, filename):
+    return "uploaded_files/%s_%s" % (str(time()).replace('.', '_'), filename)
+
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.CharField(max_length=255, verbose_name='Company Name', null=True, blank=True)
+    location = models.CharField(max_length=100, default='')
+
+    class Meta:
+        verbose_name_plural = 'Company'
+        verbose_name = 'Company'
+        db_table = 'company'
+
+    def __str__(self):
+        return str(self.company_name)
+
+
 class Talent(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    talent_name = models.CharField(max_length=100, verbose_name='Talent Name', null=False, blank=False,
+                                   default='')
+    company = models.ManyToManyField(Company)
     recruiter = models.ForeignKey(User, null=False, verbose_name='Recruiter ID')
     project = models.ForeignKey(Projects, null=True, verbose_name='Project Name')
-    # company = models.ForeignKey(Company)
     current_location = models.CharField(max_length=255, verbose_name='Current Location', null=True, blank=True)
     create_date = models.DateTimeField(verbose_name='CreateDate', null=True, blank=True)
 
@@ -70,26 +90,23 @@ class Talent(models.Model):
         db_table = 'talent'
 
     def __str__(self):
-        return str(self.id)
+        return str(self.talent_name)
 
     @property
     def get_date(self):
         return self.create_date.date().strftime('%d/%m/%Y')
 
-#
-# class Company(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     company_name = models.CharField(max_length=255, verbose_name='Company Name', null=True, blank=True)
-#
-#     class Meta:
-#         verbose_name_plural = 'Company'
-#         verbose_name = 'Company'
-#         db_table = 'company'
-#
-#     def __str__(self):
-#         return str(self.id)
 
+class TalentCompany(models.Model):
+    talent = models.ForeignKey(Talent, null=False, blank=True, verbose_name='Talent')
+    company = models.ForeignKey(Company, null=False, blank=True, verbose_name='Company')
+    start_date = models.DateField(verbose_name='Start Date', null=True, blank=True)
+    end_date = models.DateField(verbose_name='End Date', null=True, blank=True)
+    is_current = models.BooleanField(verbose_name='Currently Working Here', default=False, null=False)
 
-def get_upload_file_name(instance, filename):
-    return "uploaded_files/%s_%s" % (str(time()).replace('.', '_'), filename)
+    class Meta:
 
+        db_table = 'talent_company_tb'
+
+    def __str__(self):
+        return str(self.talent.talent_name + " works at " + self.company.company_name)
