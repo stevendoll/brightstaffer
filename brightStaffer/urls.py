7d3338374e34bbf415b0a5e361c99fe6bb2732b4
chandankiwitech <chandanvarma2@gmail.com>
2017-02-27 18:16:45 +0530

    Api changes

--- brightStaffer/urls.py
+++ brightStaffer/urls.py
@@ -20,20 +20,20 @@ from brightStafferapp import views, resetpassword
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^user_account/', views.UserData.user_account, name='user_signup'),
-    url(r'^user_login/', views.UserData.user_login, name='user_login'),
+    url(r'^user_account/', views.UserData.as_view(), name='user_signup'),
+    url(r'^user_login/', views.UserLogin.as_view(), name='user_login'),
     url(r'^forget/$', resetpassword.ForgetPassword.forget, name='forget_password'),
     url(r'^account/reset_password_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         resetpassword.ResetPassword.passwordresetconfirmView, name='reset_password_confirm'),
     url(r'^resetapi/$', resetpassword.ResetPassword.resetpasswordApi, name='reset_password_api'),
-    url(r'^job_posting/$', views.JobPosting.job_posting, name='JobPosting'),
+    url(r'^job_posting/$', views.JobPosting.as_view(), name='JobPosting'),
     url(r'^alchemy_analysis/$', views.AlchemyAPI.as_view(), name='JobPosting'),
-    url(r'^update_concept/$', views.JobPosting.update_concept, name='Update Concept'),
-    url(r'^backbuttoninfo/$', views.JobPosting.backButtonInfo, name='Back Button Info'),
-    url(r'^publish_jobPost/$', views.JobPosting.publish, name='Publish Project'),
+    url(r'^update_concept/$', views.AlchemyAPI.update_concept, name='Update Concept'),
+    url(r'^backbuttoninfo/$', views.BackButtonInfo.as_view(), name='Back Button Info'),
+    url(r'^publish_jobPost/$', views.Publish.as_view(), name='Publish Project'),
     url(r'^project_list/$', views.ProjectList.as_view()),
     url(r'^top_project_list/$', views.TopProjectList.as_view()),
     url(r'^upload/$', views.FileUpload.as_view(), name="file-upload"),
-    url(r'', views.UserData.home),
+    url(r'', views.home),
 
 ]
--- brightStafferapp/views.py
+++ brightStafferapp/views.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from watson_developer_cloud import AlchemyLanguageV1
 from django.conf import settings
 from django.contrib.auth import authenticate
-# from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
@@ -13,34 +12,29 @@ from brightStafferapp.models import Projects,Concept
 from brightStafferapp import util
 from brightStafferapp.util import require_post_params
 from brightStaffer.settings import Alchemy_api_key
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 import ast
 from itertools import chain
-# from collections import OrderedDict
 from brightStafferapp.serializers import ProjectSerializer, TopProjectSerializer, UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
-# from rest_framework import permissions
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework.reverse import reverse
-# from rest_framework import renderer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
-# from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from django.views.generic import View
 from django.utils.decorators import method_decorator
+from uuid import UUID
 
 
-class UserData(generics.ListCreateAPIView):
-    queryset = Projects.objects.all()
-    serializer_class = UserSerializer
-    # SignupAPI
-    @api_view(['POST'])
-    def user_account(request):
+class UserData(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserData, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return HttpResponse("405 ERROR:-Method is not allowed")
+
+    def post(self, request):
         param_dict = {}
         profile_data = json.loads(request.body.decode("utf-8"))
         try:
@@ -58,9 +52,15 @@ class UserData(generics.ListCreateAPIView):
         return util.returnSuccessShorcut(param_dict)
 
 
-    # Login API
-    @csrf_exempt
-    def user_login(request):
+class UserLogin(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserLogin, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return HttpResponse("405 ERROR:-Method is not allowed")
+
+    def post(self, request):
         param_dict = {}
         try:
             user_data = json.loads(request.body.decode("utf-8"))
@@ -80,82 +80,74 @@ class UserData(generics.ListCreateAPIView):
             return util.returnErrorShorcut(401, 'UnAuthorized User')
         return util.returnSuccessShorcut(param_dict)
 
-    def home(request):
-        return render(request, 'index.html', {'STATIC_URL': settings.STATIC_URL})
+
+def home(request):
+    return render(request, 'index.html', {'STATIC_URL': settings.STATIC_URL})
 
 
-class JobPosting():
-    @csrf_exempt
-    def job_posting(request):
+class JobPosting(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(JobPosting, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return HttpResponse("405 ERROR:-Method is not allowed")
+
+    def post(self, request):
         param_dict = {}
-        try:
-            user_data = json.loads(request.body.decode("utf-8"))
-        except ValueError:
-            return util.returnErrorShorcut(400, 'API parameter is not valid')
-        recruiter_email = User.objects.filter(email=user_data['recruiter'])
-        recruiter_email_valid = User.objects.filter(email=user_data['recruiter']).exists()
+        user_data = json.loads(request.body.decode("utf-8"))
         projects = Projects()
-        if recruiter_email_valid == False:
-            return util.returnErrorShorcut(404, 'Recruiter email id is not valid')
-        values = Token.objects.filter(user=recruiter_email, key=user_data['token']).select_related().exists()
-        if values == False:
-            return util.returnErrorShorcut(404, 'Access Token is not valid')
+        check_auth = user_validation(user_data)
+        if not check_auth:
+            return util.returnErrorShorcut(403, 'Either Recruiter Email or Token id is not valid')
+        if user_data['id'] == '':
+            rec_name = User.objects.get(username=user_data['recruiter'])
+            project_valid = Projects.objects.filter(project_name=user_data['project_name'],
+                                                    recruiter=rec_name).exists()
+            if not project_valid:
+                projects.project_name = user_data['project_name']
+                projects.recruiter = rec_name
+                projects.is_published = user_data['is_published']
+                projects.save()
+                p_id = Projects.objects.filter(project_name=user_data['project_name'],
+                                               recruiter=rec_name).values('id')
+                for a_id in p_id:
+                    for item, values in a_id.items():
+                        param_dict['project_id'] = str(values)
+                        return util.returnSuccessShorcut(param_dict)
+            else:
+                return util.returnErrorShorcut(403, 'Project is already exist in database')
         else:
-            if user_data['id'] == '':
-                try:
-                    rec_name = User.objects.get(username=user_data['recruiter'])
-                    pname_valid = Projects.objects.filter(project_name=user_data['project_name'],recruiter=rec_name).exists()
-                    if pname_valid == False:
-                        projects.project_name = user_data['project_name']
-                        projects.recruiter = rec_name
-                        projects.is_published = user_data['is_published']
-                        projects.save()
-                        p_id = Projects.objects.filter(project_name=user_data['project_name'],
-                                                       recruiter=rec_name).values('id')
-                        for a_id in p_id:
-                            for item, values in a_id.items():
-                                param_dict['project_id'] = str(values)
-                                return util.returnSuccessShorcut(param_dict)
-                    else:
-                        return util.returnErrorShorcut(403, 'Project is already exist in database')
-                except:
-                    return util.returnErrorShorcut(400, 'API parameter is not valid')
-
+            project_id = Projects.objects.filter(id=user_data['id']).exists()
+            if not project_id:
+                return util.returnErrorShorcut(404, 'Project id is not valid')
             else:
                 try:
-                    project_id = Projects.objects.filter(id=user_data['id']).exists()
-                    if project_id == False:
-                        return util.returnErrorShorcut(404, 'Project id is not valid')
-                    else:
-                        try:
-                            del user_data['token']
-                            del user_data['recruiter']
-                        except KeyError:
-                            pass
-                        Projects.objects.filter(id=user_data['id']).update(**user_data)
-                        return util.returnSuccessShorcut(param_dict)
-                except:
-                    return util.returnErrorShorcut(400, 'API parameter is not valid')
+                    del user_data['token']
+                    del user_data['recruiter']
+                except KeyError:
+                    pass
+                Projects.objects.filter(id=user_data['id']).update(**user_data)
+                return util.returnSuccessShorcut(param_dict)
 
-    # This API is returned a previous page info
-    @csrf_exempt
-    def backButtonInfo(request):
+
+# This API is returned a previous page info
+class BackButtonInfo(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BackButtonInfo, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return HttpResponse("405 ERROR:-Method is not allowed")
+
+    def post(self, request):
+        user_data = json.loads(request.body.decode("utf-8"))
         param_dict = {}
-        try:
-            user_data = json.loads(request.body.decode("utf-8"))
-        except ValueError:
-            return util.returnErrorShorcut(400, 'API parameter is not valid')
-        recuriter_email = User.objects.filter(email=user_data['recruiter'])
-        if not recuriter_email:
-            return util.returnErrorShorcut(404, 'Recruiter email id is not valid')
-        values = Token.objects.filter(user=recuriter_email, key=user_data['token'])  # .select_related().exists()
-        if not values:
-            return util.returnErrorShorcut(404, 'Access Token is not valid')
-        try:
-            project_id = Projects.objects.filter(id=user_data['id'])
-            if not project_id:
-                return util.returnErrorShorcut(400, 'Project id is not valid')
-        except:
+        check_auth = user_validation(user_data)
+        if not check_auth:
+            return util.returnErrorShorcut(403, 'Either Recruiter Email or Token id is not valid')
+        p_check = validate_project_by_id(user_data)
+        if not p_check:
             return util.returnErrorShorcut(400, 'Project id is not valid')
         param_dict['project_id'] = user_data['id']
         param_dict['user_token'] = user_data['token']
@@ -163,37 +155,34 @@ class JobPosting():
         value = Projects.objects.filter(id=user_data['id']).values()
         for param_value in value:
             if user_data['page'] == 1:
-                param_dict['company_name']=param_value['company_name']
-                param_dict['project_name']=param_value['project_name']
-                param_dict['location']=param_value['location']
+                param_dict['company_name'] = param_value['company_name']
+                param_dict['project_name'] = param_value['project_name']
+                param_dict['location'] = param_value['location']
             if user_data['page'] == 2:
-                param_dict['description']=param_value['description']
+                param_dict['description'] = param_value['description']
             if user_data['page'] == 3:
                 concept_dict = Concept.objects.filter(project=user_data['id']).values('concept')
                 for concept_key in concept_dict:
                     param_dict['concept'] = concept_key['concept']
-
         return util.returnSuccessShorcut(param_dict)
 
-    # This API is publish a project
-    @csrf_exempt
-    def publish(request):
+
+class Publish(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Publish, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return HttpResponse("405 ERROR:-Method is not allowed")
+
+    def post(self, request):
         param_dict = {}
-        try:
-            user_data = json.loads(request.body.decode("utf-8"))
-        except ValueError:
-            return util.returnErrorShorcut(400, 'API parameter is not valid')
-        recuriter_email = User.objects.filter(email=user_data['recruiter'])
-        if not recuriter_email:
-            return util.returnErrorShorcut(404, 'Recruiter email id is not valid')
-        values = Token.objects.filter(user=recuriter_email, key=user_data['token'])  # .select_related().exists()
-        if not values:
-            return util.returnErrorShorcut(404, 'Access Token is not valid')
-        try:
-            project_id = Projects.objects.filter(id=user_data['id'])
-            if not project_id:
-                return util.returnErrorShorcut(400, 'Project id is not valid')
-        except:
+        user_data = json.loads(request.body.decode("utf-8"))
+        check_auth = user_validation(user_data)
+        if not check_auth:
+            return util.returnErrorShorcut(403, 'Either Recruiter Email or Token id is not valid')
+        project_id = validate_project_by_id(user_data)
+        if not project_id:
             return util.returnErrorShorcut(400, 'Project id is not valid')
         try:
             del user_data['token']
@@ -204,45 +193,9 @@ class JobPosting():
         Projects.objects.filter(id=user_data['id']).update(create_date=timezone.now())
         return util.returnSuccessShorcut(param_dict)
 
-    # This API is update a concepts
-    @csrf_exempt
-    def update_concept(request):
-        param_dict = {}
-        try:
-            user_data = json.loads(request.body.decode("utf-8"))
-        except ValueError:
-            return util.returnErrorShorcut(400, 'API parameter is not valid')
-        validate_email_and_token(user_data)
-        project_id = validate_project_by_id(user_data)
-        concept_obj, created = Concept.objects.get_or_create(project_id=project_id)
-        concept_obj.concept = user_data['concept']
-        concept_obj.save()
-        param_dict['concept'] = concept_obj.concept
-        return util.returnSuccessShorcut(param_dict)
-
-
-def validate_project_by_id(request_data):
-    try:
-        project_id = Projects.objects.get(id=request_data['id'])
-        if not project_id:
-            return util.returnErrorShorcut(400, 'Project id is not valid')
-        return project_id
-    except:
-        return util.returnErrorShorcut(400, 'Project id is not valid')
-
-
-def validate_email_and_token(request_data):
-    recruiter_email = User.objects.filter(email=request_data['recruiter'])
-    if not recruiter_email:
-        return util.returnErrorShorcut(404, 'Recruiter email id is not valid')
-    values = Token.objects.filter(user=recruiter_email, key=request_data['token'])  # .select_related().exists()
-    if not values:
-        return util.returnErrorShorcut(404, 'Access Token is not valid')
-
 
 # This Class will take a input as a job description and it will return the output as a concepts(skills)
 class AlchemyAPI(View):
-
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(AlchemyAPI, self).dispatch(request, *args, **kwargs)
@@ -255,15 +208,15 @@ class AlchemyAPI(View):
         """
         context = dict()
         concept_obj = None
-        try:
-            user_data = json.loads(request.body.decode("utf-8"))
-        except ValueError:
-            return util.returnErrorShorcut(400, 'API parameter is not valid')
-        validate_email_and_token(user_data)
+        user_data = json.loads(request.body.decode("utf-8"))
+        check_auth = user_validation(user_data)
+        if not check_auth:
+            return util.returnErrorShorcut(403, 'Either Recruiter Email or Token id is not valid')
         project_id = validate_project_by_id(user_data)
-
+        if not project_id:
+            return util.returnErrorShorcut(400, 'Project id is not valid')
         if project_id:
-            project_id = str(project_id)
+            project_id = str(Projects.objects.get(id=user_data['id']))
             concept_obj, created = Concept.objects.get_or_create(project_id=project_id)
             Projects.objects.filter(id=project_id).update(description=user_data['description'])
             context['project_id'] = str(project_id)
@@ -282,6 +235,7 @@ class AlchemyAPI(View):
         context['concept'] = keyword_concepts
         return util.returnSuccessShorcut(context)
 
+    #@staticmethod
     def alchemy_api(self, user_data, project_id):
         keyword_list = []
         try:
@@ -297,6 +251,25 @@ class AlchemyAPI(View):
             return list(set(keyword_list))[:25]
         except Exception as e:
             return keyword_list
+
+            # This API is update a concepts
+
+    @csrf_exempt
+    def update_concept(self, request):
+        param_dict = {}
+        try:
+            user_data = json.loads(request.body.decode("utf-8"))
+        except ValueError:
+            return util.returnErrorShorcut(400, 'API parameter is not valid')
+        check_auth = user_validation(user_data)
+        if not check_auth:
+            return util.returnErrorShorcut(403, 'Either Recruiter Email or Token id is not valid')
+        project_id = validate_project_by_id(user_data)
+        concept_obj, created = Concept.objects.get_or_create(project_id=project_id)
+        concept_obj.concept = user_data['concept']
+        concept_obj.save()
+        param_dict['concept'] = concept_obj.concept
+        return util.returnSuccessShorcut(param_dict)
 
 
 class LargeResultsSetPagination(PageNumberPagination):
@@ -370,14 +343,6 @@ class TopProjectList(generics.ListCreateAPIView):
         return response
 
 
-def user_validation(data):
-    values = Token.objects.filter(user__username=data['recruiter'], key=data['token'])
-    if not values:
-        return False
-    else:
-        return True
-
-
 class FileUpload(View):
 
     @method_decorator(csrf_exempt)
@@ -385,7 +350,6 @@ class FileUpload(View):
         return super(FileUpload, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        #print (request.body.datya)
         files = request.FILES.getlist('file')
         dest_path = settings.MEDIA_URL
         if not os.path.exists(dest_path):
@@ -395,6 +359,7 @@ class FileUpload(View):
         context = dict()
         return util.returnSuccessShorcut(context)
 
+    #@staticmethod
     def handle_uploaded_file(self, dest_path, f):
         file_obj = open(dest_path + f.name, 'wb+')
         for chunk in f.chunks():
@@ -402,3 +367,26 @@ class FileUpload(View):
             file_obj.close()
 
 
+def user_validation(data):
+    values = Token.objects.filter(user__username=data['recruiter'], key=data['token'])
+    if not values:
+        return False
+    else:
+        return True
+
+
+def validate_project_by_id(request_data):
+    try:
+        UUID(request_data['id'], version=4)
+    except ValueError:
+        # If it's a value error, then the string
+        # is not a valid hex code for a UUID.
+        return False
+    p_id = Projects.objects.filter(id=request_data['id'])
+    if not p_id:
+        return False
+        # If it's a value error, then the string
+        # is not a valid project id.
+    else:
+        # Return True if the project id is valid
+        return True
