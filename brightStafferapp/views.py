7687ba097c35f665b4c05295d9d7ab6dfacf06af
Chandan Varma <chandan.varma@kiwitech>
2017-03-28 19:38:52 +0530

    Disable Resume Parser

--- brightStafferapp/views.py
+++ brightStafferapp/views.py
@@ -24,7 +24,7 @@ from rest_framework.response import Response
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 from uuid import UUID
-from ResumeParser.core import create_resume
+# from ResumeParser.core import create_resume
 import PyPDF2
 from PIL import Image
 import os
