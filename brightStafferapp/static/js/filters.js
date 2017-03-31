9cc450ee2d6d25dd4f995878655c256cd662c250
rajesh-kiwi <rajesh.dobhal@kiwitech.com>
2017-03-31 14:47:32 +0530

    null issue fixed

--- brightStafferapp/static/js/filters.js
+++ brightStafferapp/static/js/filters.js
@@ -1,69 +1,69 @@
-function dateFormat($filter)
-{
- return function(input)
- {
-  if(input == null){ return ""; }
-     input = input.split('/');
-     input =  input[1]+'/'+input[0]+'/'+input[2];
-  var _date = $filter('date')(new Date(input), 'MMM dd, yyyy');
+function dateFormat($filter) {
+    return function (input) {
+        if (input == null) {
+            return "";
+        }
+        input = input.split('/');
+        input = input[1] + '/' + input[0] + '/' + input[2];
+        var _date = $filter('date')(new Date(input), 'MMM dd, yyyy');
 
-  return _date;
+        return _date;
 
- };
+    };
 }
 
-function yearFormat($filter)
-{
- return function(input)
- {
-  if(input == null){ return ""; }
-     input = input.split('/');
-     input =  input[1]+'/'+input[0]+'/'+input[2];
-  var _date = $filter('date')(new Date(input), 'yyyy');
+function yearFormat($filter) {
+    return function (input) {
+        if (!input) {
+            return "";
+        }
+        input = input.split('/');
+        input = input[1] + '/' + input[0] + '/' + input[2];
+        var _date = $filter('date')(new Date(input), 'yyyy');
 
-  return _date;
+        return _date;
 
- };
+    };
 }
 
 
 function capitalize() {
-    return function(input) {
-      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    return function (input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     }
 }
 
 function capitalizeAll() {
-    return function(input) {
-      return (!!input) ? input.toUpperCase() : '';
+    return function (input) {
+        return (!!input) ? input.toUpperCase() : '';
     }
 }
 
 function capitalizeWord() {
-  return function(input){
-    if(input){
-    if(input.indexOf(' ') !== -1){
-      var inputPieces,
-          i;
+    return function (input) {
+        if (input) {
+            if (input.indexOf(' ') !== -1) {
+                var inputPieces
+                    , i;
 
-      input = input.toLowerCase();
-      inputPieces = input.split(' ');
+                input = input.toLowerCase();
+                inputPieces = input.split(' ');
 
-      for(i = 0; i < inputPieces.length; i++){
-        inputPieces[i] = capitalizeString(inputPieces[i]);
-      }
+                for (i = 0; i < inputPieces.length; i++) {
+                    inputPieces[i] = capitalizeString(inputPieces[i]);
+                }
 
-      return inputPieces.toString().replace(/,/g, ' ');
-    }
-    else {
-      input = input.toLowerCase();
-      return capitalizeString(input);
-    }
-    }
-    function capitalizeString(inputString){
-      return inputString.substring(0,1).toUpperCase() + inputString.substring(1);
-    }
-  };
+                return inputPieces.toString().replace(/,/g, ' ');
+            } else {
+                input = input.toLowerCase();
+                return capitalizeString(input);
+            }
+        }
+
+        function capitalizeString(inputString) {
+            return inputString.substring(0, 1).toUpperCase() + inputString.substring(1);
+        }
+    };
 }
 
 function sumOfValue() {
@@ -71,7 +71,7 @@ function sumOfValue() {
         if (angular.isUndefined(data) || angular.isUndefined(key))
             return 0;
         var sum = 0;
-        angular.forEach(data,function(value){
+        angular.forEach(data, function (value) {
             sum = sum + parseInt(value[key]);
         });
         return sum;
@@ -79,22 +79,22 @@ function sumOfValue() {
 }
 
 function dateDiff() {
-  var magicNumber = (1000 * 60 * 60 * 24);
+    var magicNumber = (1000 * 60 * 60 * 24);
 
-  return function (toDate, fromDate) {
-    if(toDate && fromDate){
-      var dayDiff = Math.floor((toDate - fromDate) / magicNumber);
-      if (angular.isNumber(dayDiff)){
-        return dayDiff + 1;
-      }
-    }
-  };
+    return function (toDate, fromDate) {
+        if (toDate && fromDate) {
+            var dayDiff = Math.floor((toDate - fromDate) / magicNumber);
+            if (angular.isNumber(dayDiff)) {
+                return dayDiff + 1;
+            }
+        }
+    };
 }
 
-function contactFormat(){
+function contactFormat() {
 
-    return function(input){
-        if(!input) return input;
+    return function (input) {
+        if (!input) return input;
         input = input.toString();
         input = input.substr(0, 3) + '-' + input.substr(3);
         input = input.substr(0, 7) + '-' + input.substr(7);
